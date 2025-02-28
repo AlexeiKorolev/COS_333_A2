46ef5349dcfbff2d590b453f60b0119f3cf04dc6
Venkat Subramanian <vs9542@princeton.edu>
2025-02-27 22:54:30 -0500

    pylint regdetails

--- regdetails.py
+++ regdetails.py
@@ -59,10 +59,10 @@ def format_reg_response(dict_results):
 
     # Formatting based on requirements
 
-"""
-Starts the client, which sends a request to the server based on the given
-arguments and then prints the results.
-"""
+# Starts the client, which sends a request
+# to the server based on the given
+# arguments and then prints the results.
+
 def main():
     # Set up help options
 
